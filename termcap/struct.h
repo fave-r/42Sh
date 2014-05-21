501f66a8ea094e66324069466079a01f4c156d80
odet_a <odet_a@epitech.eu>
2014-05-21 16:24:32 +0200

    Ajout de la structure

---
+++ termcap/struct.h
@@ -0,0 +1,18 @@
+/*
+** struct.h for struct in /Users/Alex/work/42Sh/termcap
+** 
+** Made by Alex
+** Login   <Alex@epitech.net>
+** 
+** Started on  Wed May 21 16:09:35 2014 Alex
+** Last update Wed May 21 16:10:36 2014 Alex
+*/
+
+#ifndef __GNL_ICANON__
+#define __GNL_ICANON__
+
+#include <termios.h>
+#include <term.h>
+#include <curses.h>
+
+#endif
