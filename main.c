4dee5e31be9951ad299603ac289e923017fc1cfb
leo <leo@epitech.eu>
2014-05-22 11:23:55 +0200

    Norme main.c

--- main.c
+++ main.c
@@ -5,19 +5,10 @@
 ** Login   <alex-odet@epitech.net>
 **
 ** Started on  Fri Apr  4 11:05:16 2014 alex-odet
-** Last update Wed May 21 19:35:38 2014 bourrel
+** Last update Thu May 22 11:22:37 2014 bourrel
 */
 
 #include "my.h"
-
-void		my_show_list(t_token *list)
-{
-  while (list)
-    {
-      printf("token = %s\n", list->token);
-      list = list->next;
-    }
-}
 
 void		display_prompt()
 {
@@ -46,13 +37,36 @@ int	check_exit(t_env *env)
   return (ret);
 }
 
+int		start(t_token *list, t_env_var env, t_tree *tree, char *tmp)
+{
+  int	ret;
+
+  ret = 0;
+  tmp = my_epur_str(tmp);
+  if (tmp && tmp[0] != 0)
+    {
+      list = fill_token(tmp);
+      tree = npi(list, tmp);
+      check_fn(tree, 0, 1, &env);
+      free_tree(tree);
+      delete_list(&list);
+      if ((ret = check_exit(env.env)) != -1)
+	{
+	  free(tmp);
+	  my_delete_envlist(&(env.env));
+	  return (ret);
+	}
+    }
+  display_prompt();
+  return (0);
+}
+
 int		main(int ac, char **av, char **envp)
 {
   t_token	*list;
   t_env_var	env;
   t_tree	*tree;
   char		*tmp;
-  int		ret;
 
   tree = NULL;
   tmp = xmalloc(4096 * sizeof(char));
@@ -65,24 +79,7 @@ int		main(int ac, char **av, char **envp)
   env.var_close = -1;
   signal(SIGINT, &display_sigint);
   while ((tmp = get_next_line_icanon(0)) != NULL)
-    {
-      tmp = my_epur_str(tmp);
-      if (tmp && tmp[0] != 0)
-	{
-	  list = fill_token(tmp);
-	  tree = npi(list, tmp);
-	  check_fn(tree, 0, 1, &env);
-	  free_tree(tree);
-	  delete_list(&list);
-	  if ((ret = check_exit(env.env)) != -1)
-	    {
-	      free(tmp);
-	      my_delete_envlist(&(env.env));
-	      return (ret);
-	    }
-	}
-      display_prompt();
-    }
+    start(list, env, tree, tmp);
   write(1, "\n", 1);
   free(tmp);
   my_delete_envlist(&(env.env));
