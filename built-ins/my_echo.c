0e949d316e4bfa8262881f730be99121ea8c097a
leo <leo@epitech.eu>
2014-05-25 20:20:48 +0200

    Echo

--- built-ins/my_echo.c
+++ built-ins/my_echo.c
@@ -5,7 +5,7 @@
 ** Login   <leo@epitech.net>
 ** 
 ** Started on  Thu Apr 24 14:30:23 2014 bourrel
-** Last update Sat May 24 16:56:37 2014 lhomme
+** Last update Sun May 25 17:56:37 2014 bourrel
 */
 
 #include "my.h"
@@ -37,6 +37,7 @@ void	print_text(char **tab, int fd)
 
 void	unknown_arg(char *text, char *arg, int i, int fd)
 {
+  printf("JNJKHUKLN\n");
   while (arg[i] != '\0')
     {
       print_echo_str(&arg[i], fd);
@@ -46,13 +47,33 @@ void	unknown_arg(char *text, char *arg, int i, int fd)
   print_echo_str(text, fd);
 }
 
-int	check_echo_args(char **tab, int fd, int x)
+int	check_invalid_arg(char *arg, char *text, int fd)
 {
   int	i;
 
   i = 0;
+  while (arg[i] != '\0')
+    {
+      if (arg[i] != 'e' || arg[i] != 'E' || arg[i] != 'n' || arg[i] != '-')
+	{
+	  print_echo_str(arg, fd);
+	  if (text)
+	    print_echo_str(text, fd);
+	  return (0);
+	}
+      i++;
+    }
+  return (1);
+}
+
+int	check_echo_args(char **tab, int fd, int x, int i)
+{
+  if (check_invalid_arg(tab[1], tab[2], fd) == 0)
+    return (x);
   if (tab[1][1] == '-')
     echo_vh(tab, fd);
+  if (tab[1][1] == '\0' && tab[2])
+    print_echo_str(tab[2], fd);
   while (tab[1][++i] != '\0')
     {
       if (tab[2] && tab[1][i] == 'e')
@@ -69,8 +90,6 @@ int	check_echo_args(char **tab, int fd, int x)
 	}
       else if (tab[1][i] == 'n')
 	x = 1;
-      else if (tab[2])
-	unknown_arg(tab[2], tab[1], i, fd);
     }
   return (x);
 }
@@ -78,14 +97,16 @@ int	check_echo_args(char **tab, int fd, int x)
 void	my_echo(char **tab, int fd)
 {
   int	x;
+  int	i;
 
+  i = 0;
   x = 0;
   if (tab[1] == NULL)
     x = 0;
   else if (tab[1][0] != '-')
     print_text(tab, fd);
   else
-    x = check_echo_args(tab, fd, x);
+    x = check_echo_args(tab, fd, x, i);
   if (x == 0)
     my_putchar('\n', fd);
 }
